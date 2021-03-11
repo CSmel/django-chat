250ffe86c20d907982a0e04e01532ece362304f0
Csmel <webdeveloper@creativesakas.com>
2021-03-10 19:35:26 -0500

    Initial commit

---
+++ djangochat/urls.py
@@ -0,0 +1,15 @@
+# mysite/urls.py
+from django.conf.urls import include
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.conf.urls.static import static
+urlpatterns = [
+    path('chat/', include('chat.urls')),
+    path('admin/', admin.site.urls),
+]
+urlpatterns += staticfiles_urlpatterns()# for js/css/sass files
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #images
